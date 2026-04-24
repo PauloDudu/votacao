2167c42fdd9eb4517f0e97e11a5faf46e0d6d920
Paulo <paulo.ferreira@alterdata.com.br>
2026-04-24 15:53:16 -0300

    Adding icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,9 @@ import './globals.css'
 export const metadata: Metadata = {
   title: 'Votação',
   description: 'Crie votações e compartilhe com qualquer pessoa',
+  icons: {
+    icon: '/icon.svg',
+  },
 }
 
 export default function RootLayout({
